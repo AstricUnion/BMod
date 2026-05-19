9aa499d1b057a7e011e0e50108147fb9e3daad46
maxobur0001 <msblock09@gmail.com>
2026-05-19 21:18:18 +0300

    Textures update

--- autorun/cl_meshload.lua
+++ autorun/cl_meshload.lua
@@ -1,11 +1,8 @@
 if SERVER then return end
 
 local armorMat = model.newMaterial("armor", "VertexLitGeneric")
-local matrix = Matrix()
-matrix:setScale(Vector(0.25, 0.25, 0))
-armorMat:setMatrix("$basetexturetransform", matrix)
-armorMat:setInt("$realwidth", 256)
-armorMat:setInt("$realheight", 256)
+armorMat:setInt("$realwidth", 1024)
+armorMat:setInt("$realheight", 1024)
 armorMat:setTextureURL("$basetexture", "https://raw.githubusercontent.com/AstricUnion/BMod/refs/heads/main/textures/armor.jpg")
 
 local armor = model.newMesh("armor", "https://raw.githubusercontent.com/AstricUnion/BMod/refs/heads/main/mesh/armor.obj")
