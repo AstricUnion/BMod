f1e8121713064c33059dfe364bcfa24f195b150f
maxobur0001 <msblock09@gmail.com>
2026-05-26 22:05:23 +0300

    Test compressed models

--- autorun/cl_meshload.lua
+++ autorun/cl_meshload.lua
@@ -6,7 +6,7 @@ mat:setScale(Vector(0.75, 0.75, 0.75))
 armorMat:setMatrix("$basetexturetransform", mat)
 armorMat:setInt("$realwidth", 1024)
 armorMat:setInt("$realheight", 1024)
-armorMat:setTextureURL("$basetexture", "https://www.dropbox.com/scl/fi/o9w9ow1zbsdzn3tuw0uv9/armor.jpg?rlkey=ib70iluie9l8sidob35h4i1e0&st=yxcdi2u8&dl=1")
+armorMat:setTextureURL("$basetexture", "https://raw.githubusercontent.com/AstricUnion/BMod/refs/heads/main/textures/armor.jpg")
 
 local armor = model.newMesh("armor", "https://raw.githubusercontent.com/AstricUnion/BMod/refs/heads/main/mesh/armor.obj")
 armor:setMaterial("armor")
