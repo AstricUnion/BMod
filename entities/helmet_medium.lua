9889b7d9c18ab690cbf6e8ab08ec0fb9057bf67d
maxobur0001 <msblock09@gmail.com>
2026-05-27 12:02:03 +0300

    Attempt to add all armor from JMod

--- entities/helmet_medium.lua
+++ entities/helmet_medium.lua
@@ -18,7 +18,7 @@ local mdl = model.new("helmet_medium", hitbox {
     vertex {"cube", Vector(0, 0, 10), Angle(0, 0, 0), Vector(6, 6, 10)},
     mass = 10
 })
-    :add("base", holo { ang = Angle(90, 0, 0), model = "models/holograms/cube.mdl", mesh = "armor", meshPart = "helmet_light"} )
+    :add("base", holo { ang = Angle(90, 0, 0), model = "models/holograms/cube.mdl", mesh = "armor", meshPart = "helmet_medium"} )
 
 ---@class HelmetMedium: Equippable
 local HelmetMedium = {}
@@ -28,7 +28,7 @@ HelmetMedium.Model = function()
     return mdl:create()
 end
 HelmetMedium.BoneToEquip = "ValveBiped.Bip01_Head1"
-HelmetMedium.EquipOffset = Vector(-2, -1, 0)
+HelmetMedium.EquipOffset = Vector(-2, 0.5, 0)
 HelmetMedium.EquipAngle = Vector(0, -90, -90)
 HelmetMedium.EquipSlots = {[EquipSlot.head] = 0.8}
 HelmetMedium.DefenseProfile = DefenseProfile.Basic
